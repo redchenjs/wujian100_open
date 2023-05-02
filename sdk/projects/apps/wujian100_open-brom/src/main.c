f235195f45070d7a43d0a4e5cb0e49534492d027
Jack Chen <redchenjs@live.com>
2023-05-02 20:03:53 +0800

    sdk: update brom files

--- sdk/projects/apps/wujian100_open-brom/src/main.c
+++ sdk/projects/apps/wujian100_open-brom/src/main.c
@@ -36,9 +36,104 @@ uint32_t get_timestamp(void)
     return tp.tv_nsec / 1000;
 }
 
+// PWM Functions
+#define PWM_TEST_CH 0
+
+pwm_handle_t pwm_handle = NULL;
+
+void pwm_init(void)
+{
+    pwm_handle = csi_pwm_initialize(PWM_TEST_CH);
+
+    csi_pwm_config(pwm_handle, PWM_TEST_CH, 2500, 0);
+}
+
+void pwm_test(void)
+{
+    static int state = 0;
+
+    if (pwm_handle) {
+        state = !state;
+
+        if (state) {
+            csi_pwm_start(pwm_handle, PWM_TEST_CH);
+        } else {
+            csi_pwm_stop(pwm_handle, PWM_TEST_CH);
+        }
+    }
+}
+
+void pwm_deinit(void)
+{
+    csi_pwm_stop(pwm_handle, PWM_TEST_CH);
+    csi_pwm_uninitialize(pwm_handle);
+}
+
+// GPIO Functions
+#define GPIO_TEST_PIN 0
+
+gpio_pin_handle_t pin_handle = NULL;
+
+void gpio_init(void)
+{
+    pin_handle = csi_gpio_pin_initialize(GPIO_TEST_PIN, NULL);
+
+    csi_gpio_pin_config_mode(pin_handle, GPIO_MODE_PULLNONE);
+    csi_gpio_pin_config_direction(pin_handle, GPIO_DIRECTION_OUTPUT);
+
+    csi_gpio_pin_write(pin_handle, 0);
+}
+
+void gpio_toggle(void)
+{
+    static int state = 0;
+
+    if (pin_handle) {
+        state = !state;
+        csi_gpio_pin_write(pin_handle, state);
+    }
+}
+
+void gpio_deinit(void)
+{
+    csi_gpio_pin_write(pin_handle, 0);
+    csi_gpio_pin_uninitialize(pin_handle);
+}
+
 int main(void)
 {
+    int firmware_ok = 0;
+    void (*firmware_entry)(void) = (void (*)())(0x80000000);
+
+    pwm_init();
+    gpio_init();
+
     printf("(%u) brom: started.\n", get_timestamp());
+
+    if (firmware_ok) {
+        printf("(%u) brom: firmware is signed, loading....\n", get_timestamp());
+
+        for (int i = 20; i > 0; i--) {
+            pwm_test();
+            gpio_toggle();
+
+            delay_ms(50);
+        }
+    } else {
+        printf("(%u) brom: firmware is not signed.\n", get_timestamp());
+
+        while (1) {
+            pwm_test();
+            gpio_toggle();
+
+            delay_ms(200);
+        }
+    }
+
+    pwm_deinit();
+    gpio_deinit();
+
+    firmware_entry();
 
     return 0;
 }
