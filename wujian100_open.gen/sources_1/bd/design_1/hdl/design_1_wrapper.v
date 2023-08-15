ffeeba1b61c48e5ace1265c967db910a7ab60ee3
Jack Chen <redchenjs@live.com>
2023-08-16 01:53:42 +0800

    misc: add cdk workspace file

--- wujian100_open.gen/sources_1/bd/design_1/hdl/design_1_wrapper.v
+++ wujian100_open.gen/sources_1/bd/design_1/hdl/design_1_wrapper.v
@@ -1,9 +1,9 @@
 //Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
 //Copyright 2022-2023 Advanced Micro Devices, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
-//Tool Version: Vivado v.2023.1.1 (win64) Build 3865809 Sun May  7 15:05:29 MDT 2023
-//Date        : Tue Aug 15 22:57:07 2023
-//Host        : ThinPC running 64-bit major release  (build 9200)
+//Tool Version: Vivado v.2023.1.1 (win64) Build 3900603 Fri Jun 16 19:31:24 MDT 2023
+//Date        : Wed Aug 16 01:08:13 2023
+//Host        : ThinkBook running 64-bit major release  (build 9200)
 //Command     : generate_target design_1_wrapper.bd
 //Design      : design_1_wrapper
 //Purpose     : IP block netlist
