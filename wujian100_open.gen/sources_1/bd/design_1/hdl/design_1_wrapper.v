56c1b7488f767dc0b5c6253d404a777d5f3230e9
Jack Chen <redchenjs@live.com>
2023-08-17 21:00:10 +0800

    misc: ZU5EV_PL

--- wujian100_open.gen/sources_1/bd/design_1/hdl/design_1_wrapper.v
+++ wujian100_open.gen/sources_1/bd/design_1/hdl/design_1_wrapper.v
@@ -2,7 +2,7 @@
 //Copyright 2022-2023 Advanced Micro Devices, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2023.1.1 (win64) Build 3900603 Fri Jun 16 19:31:24 MDT 2023
-//Date        : Wed Aug 16 23:16:24 2023
+//Date        : Thu Aug 17 16:22:22 2023
 //Host        : ThinkBook running 64-bit major release  (build 9200)
 //Command     : generate_target design_1_wrapper.bd
 //Design      : design_1_wrapper
@@ -11,22 +11,7 @@
 `timescale 1 ps / 1 ps
 
 module design_1_wrapper
-   (DDR_addr,
-    DDR_ba,
-    DDR_cas_n,
-    DDR_ck_n,
-    DDR_ck_p,
-    DDR_cke,
-    DDR_cs_n,
-    DDR_dm,
-    DDR_dq,
-    DDR_dqs_n,
-    DDR_dqs_p,
-    DDR_odt,
-    DDR_ras_n,
-    DDR_reset_n,
-    DDR_we_n,
-    EXT_BEEP,
+   (EXT_BEEP,
     EXT_JTAG_TCK_0,
     EXT_JTAG_TCK_1,
     EXT_JTAG_TMS_0,
@@ -55,29 +40,9 @@ module design_1_wrapper
     EXT_UART_TXD_0,
     EXT_UART_TXD_1,
     EXT_WS2812,
-    FIXED_IO_ddr_vrn,
-    FIXED_IO_ddr_vrp,
-    FIXED_IO_mio,
-    FIXED_IO_ps_clk,
-    FIXED_IO_ps_porb,
-    FIXED_IO_ps_srstb,
-    PL_CLK_32K,
-    PL_CLK_50M);
-  inout [14:0]DDR_addr;
-  inout [2:0]DDR_ba;
-  inout DDR_cas_n;
-  inout DDR_ck_n;
-  inout DDR_ck_p;
-  inout DDR_cke;
-  inout DDR_cs_n;
-  inout [3:0]DDR_dm;
-  inout [31:0]DDR_dq;
-  inout [3:0]DDR_dqs_n;
-  inout [3:0]DDR_dqs_p;
-  inout DDR_odt;
-  inout DDR_ras_n;
-  inout DDR_reset_n;
-  inout DDR_we_n;
+    PL_CLK_200M_N,
+    PL_CLK_200M_P,
+    PL_CLK_32K);
   inout EXT_BEEP;
   inout EXT_JTAG_TCK_0;
   inout EXT_JTAG_TCK_1;
@@ -107,30 +72,10 @@ module design_1_wrapper
   inout EXT_UART_TXD_0;
   inout EXT_UART_TXD_1;
   output EXT_WS2812;
-  inout FIXED_IO_ddr_vrn;
-  inout FIXED_IO_ddr_vrp;
-  inout [53:0]FIXED_IO_mio;
-  inout FIXED_IO_ps_clk;
-  inout FIXED_IO_ps_porb;
-  inout FIXED_IO_ps_srstb;
+  input PL_CLK_200M_N;
+  input PL_CLK_200M_P;
   input PL_CLK_32K;
-  input PL_CLK_50M;
 
-  wire [14:0]DDR_addr;
-  wire [2:0]DDR_ba;
-  wire DDR_cas_n;
-  wire DDR_ck_n;
-  wire DDR_ck_p;
-  wire DDR_cke;
-  wire DDR_cs_n;
-  wire [3:0]DDR_dm;
-  wire [31:0]DDR_dq;
-  wire [3:0]DDR_dqs_n;
-  wire [3:0]DDR_dqs_p;
-  wire DDR_odt;
-  wire DDR_ras_n;
-  wire DDR_reset_n;
-  wire DDR_we_n;
   wire EXT_BEEP;
   wire EXT_JTAG_TCK_0;
   wire EXT_JTAG_TCK_1;
@@ -160,32 +105,12 @@ module design_1_wrapper
   wire EXT_UART_TXD_0;
   wire EXT_UART_TXD_1;
   wire EXT_WS2812;
-  wire FIXED_IO_ddr_vrn;
-  wire FIXED_IO_ddr_vrp;
-  wire [53:0]FIXED_IO_mio;
-  wire FIXED_IO_ps_clk;
-  wire FIXED_IO_ps_porb;
-  wire FIXED_IO_ps_srstb;
+  wire PL_CLK_200M_N;
+  wire PL_CLK_200M_P;
   wire PL_CLK_32K;
-  wire PL_CLK_50M;
 
   design_1 design_1_i
-       (.DDR_addr(DDR_addr),
-        .DDR_ba(DDR_ba),
-        .DDR_cas_n(DDR_cas_n),
-        .DDR_ck_n(DDR_ck_n),
-        .DDR_ck_p(DDR_ck_p),
-        .DDR_cke(DDR_cke),
-        .DDR_cs_n(DDR_cs_n),
-        .DDR_dm(DDR_dm),
-        .DDR_dq(DDR_dq),
-        .DDR_dqs_n(DDR_dqs_n),
-        .DDR_dqs_p(DDR_dqs_p),
-        .DDR_odt(DDR_odt),
-        .DDR_ras_n(DDR_ras_n),
-        .DDR_reset_n(DDR_reset_n),
-        .DDR_we_n(DDR_we_n),
-        .EXT_BEEP(EXT_BEEP),
+       (.EXT_BEEP(EXT_BEEP),
         .EXT_JTAG_TCK_0(EXT_JTAG_TCK_0),
         .EXT_JTAG_TCK_1(EXT_JTAG_TCK_1),
         .EXT_JTAG_TMS_0(EXT_JTAG_TMS_0),
@@ -214,12 +139,7 @@ module design_1_wrapper
         .EXT_UART_TXD_0(EXT_UART_TXD_0),
         .EXT_UART_TXD_1(EXT_UART_TXD_1),
         .EXT_WS2812(EXT_WS2812),
-        .FIXED_IO_ddr_vrn(FIXED_IO_ddr_vrn),
-        .FIXED_IO_ddr_vrp(FIXED_IO_ddr_vrp),
-        .FIXED_IO_mio(FIXED_IO_mio),
-        .FIXED_IO_ps_clk(FIXED_IO_ps_clk),
-        .FIXED_IO_ps_porb(FIXED_IO_ps_porb),
-        .FIXED_IO_ps_srstb(FIXED_IO_ps_srstb),
-        .PL_CLK_32K(PL_CLK_32K),
-        .PL_CLK_50M(PL_CLK_50M));
+        .PL_CLK_200M_N(PL_CLK_200M_N),
+        .PL_CLK_200M_P(PL_CLK_200M_P),
+        .PL_CLK_32K(PL_CLK_32K));
 endmodule
